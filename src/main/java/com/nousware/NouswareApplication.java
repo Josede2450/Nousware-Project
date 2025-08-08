24193490e3eca13f7c2d8ade96101cddb4e0ab91
Jose De Jesus <josegabrieldejesus@hotmail.com>
2025-08-07 23:19:04 -0400

    Initial commit

---
+++ src/main/java/com/nousware/NouswareApplication.java
@@ -0,0 +1,13 @@
+package com.nousware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NouswareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NouswareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nousware/NouswareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nousware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NouswareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
